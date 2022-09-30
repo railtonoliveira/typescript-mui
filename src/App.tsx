87bafee88771a053a7427d90373cede0061ee8bc
Railton Oliveira da Silva <railtonoliiveira@gmail.com>
2022-09-29 20:20:10 -0400

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-
+import { BrowserRouter } from "react-router-dom"; 
+import { AppRoutes } from "./routes";
 
 export const App = () => {
 
   return (
-    <div>
-      <h1>Meu app</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
